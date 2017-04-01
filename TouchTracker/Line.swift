b14ce1058bd83651d8d2b168af1e1089052a45c9
Alex Louzao <alexlouzao@Alexs-MacBook-Pro.local>
2017-04-01 12:56:02 -0400

    ch 18 done no challenges

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Alex Louzao on 4/1/17.
+//  Copyright © 2017 ALcsc2310. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line{
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
